7a1c6cf877d0f56e4285bbde58981365b8813366
dryzhik <91097017+dryzhik@users.noreply.github.com>
2023-12-28 23:55:51 +0300

    fix client

--- client-server/client.py
+++ client-server/client.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import csv
+import argparse
 
 
 async def send_request(url, method='GET', data=None):
@@ -19,16 +20,13 @@ async def send_request(url, method='GET', data=None):
                 return None, response.status
 
 
-async def main():
+async def create_new_lab(name, deadline, description, students):
     base_url = 'http://127.0.0.1:8080/labs'
-    csv_filename_all = 'labs.csv'
-    csv_filename_single = 'lab.csv'
-
     new_lab_data = {
-        'name': 'Lab1',
-        'deadline': '01.01.2024',
-        'description': 'Description for Lab1',
-        'students': ['Ivanov', 'Petrov']
+        'name': name,
+        'deadline': deadline,
+        'description': description,
+        'students': students
     }
     response_data, status = await send_request(base_url, method='POST', data=new_lab_data)
     if status // 100 == 2:
@@ -36,52 +34,49 @@ async def main():
     else:
         print(f"Failed to add lab. Status: {status}, Reason: {response_data['error']}")
 
+
+async def edit_lab(name, deadline, description, students):
+    base_url = f'http://127.0.0.1:8080/labs/{name}'
     edit_lab_data = {
-        'deadline': '02.01.2024',
-        'description': 'Updated description for Lab1'
+        'deadline': deadline,
+        'description': description,
+        'students': students
     }
-    edit_url = f"{base_url}/Lab1"
-    response_data, status = await send_request(edit_url, method='PUT', data=edit_lab_data)
+    response_data, status = await send_request(base_url, method='PUT', data=edit_lab_data)
     if status // 100 == 2:
         print("Lab edited successfully.")
     else:
         print(f"Failed to edit lab. Status: {status}, Reason: {response_data['error']}")
 
-    delete_url = f"{base_url}/Lab1"
-    response_data, status = await send_request(delete_url, method='DELETE')
+
+async def delete_lab(name):
+    base_url = f'http://127.0.0.1:8080/labs/{name}'
+    response_data, status = await send_request(base_url, method='DELETE')
     if status // 100 == 2:
         print("Lab deleted successfully.")
     else:
         print(f"Failed to delete lab. Status: {status}, Reason: {response_data['error']}")
 
-    single_lab_url = f"{base_url}/Lab1"
-    response_data, status = await send_request(single_lab_url)
+
+async def get_lab(name, csv_filename):
+    base_url = f'http://127.0.0.1:8080/labs/{name}'
+    response_data, status = await send_request(base_url)
     if status // 100 == 2:
         print(f"Lab Data: {response_data}")
-        with open(csv_filename_single, 'w', newline='') as csv_file:
+        with open(csv_filename, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(['Name', 'Deadline', 'Description'])
             csv_writer.writerow([response_data['name'], response_data['deadline'], response_data['description']])
     else:
         print(f"Failed to get lab data. Status: {status}, Reason: {response_data['error']}")
 
-    new_lab2_data = {
-        'name': 'Lab3',
-        'deadline': '07.02.2024',
-        'description': 'Description for Lab3',
-        'students': ['Ivanov', 'Petrov']
-    }
-    response_data, status = await send_request(base_url, method='POST', data=new_lab2_data)
-    if status // 100 == 2:
-        print(f"Lab added successfully. URL: {response_data['url']}")
-    else:
-        print(f"Failed to add lab. Status: {status}, Reason: {response_data['error']}")
 
-    all_labs_url = f"{base_url}/"
-    response_data, status = await send_request(all_labs_url)
+async def get_all_labs(csv_filename):
+    base_url = f'http://127.0.0.1:8080/labs/'
+    response_data, status = await send_request(base_url)
     if status // 100 == 2:
         print(f"All Labs Data: {response_data}")
-        with open(csv_filename_all, 'w', newline='') as csv_file:
+        with open(csv_filename, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(['Student', 'Lab Name', 'Deadline', 'Description'])
             for lab in response_data:
@@ -89,6 +84,28 @@ async def main():
                     csv_writer.writerow([lab['students'][i], lab['name'], lab['deadline'], lab['description']])
     else:
         print(f"Failed to get all labs data. Status: {status}, Reason: {response_data['error']}")
+
+
+async def main():
+    parser = argparse.ArgumentParser(description='Lab Client')
+    parser.add_argument('command', choices=['new_lab', 'edit_lab', 'delete_lab', 'get_lab', 'get_all_labs'],
+                        help='The command to execute')
+    parser.add_argument('--name', help='Lab name')
+    parser.add_argument('--deadline', help='Lab deadline')
+    parser.add_argument('--description', help='Lab description')
+    parser.add_argument('--students', nargs='+', help='List of students')
+
+    args = parser.parse_args()
+    if args.command == 'new_lab':
+        await create_new_lab(args.name, args.deadline, args.description, args.students)
+    elif args.command == 'edit_lab':
+        await edit_lab(args.name, args.deadline, args.description, args.students)
+    elif args.command == 'delete_lab':
+        await delete_lab(args.name)
+    elif args.command == 'get_lab':
+        await get_lab(args.name, args.name + '.csv')
+    elif args.command == 'get_all_labs':
+        await get_all_labs(args.name + '.csv')
 
 
 if __name__ == '__main__':
